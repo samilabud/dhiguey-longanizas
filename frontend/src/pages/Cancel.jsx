421982024c23aef0b3decf0c6f8ec24e8c7deb5c
Samil Abud <samilabud@gmail.com>
2025-02-07 16:48:50 -0400

    feature: Add success confirmation and new confirmation messages

--- frontend/src/pages/Cancel.jsx
+++ frontend/src/pages/Cancel.jsx
@@ -4,8 +4,12 @@ const Cancel = () => {
   return (
     <Layout>
       <h1 className="text-red-600 text-center text-2xl mt-6">
-        Payment Canceled.
+        ❌ Pago cancelado
       </h1>
+      <p className="text-center mt-2">
+        Tu pago no se ha procesado. Si crees que esto es un error, inténtalo de
+        nuevo o contáctanos.
+      </p>
     </Layout>
   );
 };
--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -1,26 +1,45 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import Layout from "./Layout";
+import { BACKEND_URL } from "../config";
 
 const Success = () => {
+  const [syncStatus, setSyncStatus] = useState(
+    "Espere mientras sincronizamos el estado de su orden..."
+  );
   const [searchParams] = useSearchParams();
   const orderID = searchParams.get("token");
 
   useEffect(() => {
-    if (orderID) {
-      axios
-        .post(`http://localhost:5000/api/paypal/capture-payment/${orderID}`)
-        .then(() => alert("Payment successful!"))
-        .catch((err) => console.error(err));
-    }
+    const capturePayment = async () => {
+      try {
+        const { data } = await axios.post(
+          `${BACKEND_URL}/api/paypal/capture-payment/${orderID}`
+        );
+        console.log("✅ Payment Captured:", data);
+        setSyncStatus("¡gracias! 🎉");
+      } catch (error) {
+        console.error("❌ Payment Capture Error:", error);
+        setSyncStatus(
+          "❌ Error al sincronizar el pago. Por favor, contáctenos."
+        );
+      }
+    };
+
+    if (orderID) capturePayment();
   }, [orderID]);
 
   return (
     <Layout>
-      <h1 className="text-green-600 text-center text-2xl mt-6">
-        Payment Successful!
-      </h1>
+      <div className="flex flex-col items-center">
+        <h1 className="text-green-600 text-center text-2xl mt-6">
+          🎉 ¡Pago exitoso!
+        </h1>
+        <h2 className="text-green-600 text-center text-2xl mt-6">
+          {syncStatus}
+        </h2>
+      </div>
     </Layout>
   );
 };
