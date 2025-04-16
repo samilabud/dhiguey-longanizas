041f0a7336b98f3a6a6c460751a5320a0b6c6bb0
Samil Abud <samilabud@gmail.com>
2025-04-15 23:27:41 -0400

    Fixes helmet issue on deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,9 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  build: {
+    rollupOptions: {
+      external: ["react-helmet-async"],
+    },
+  },
 });
